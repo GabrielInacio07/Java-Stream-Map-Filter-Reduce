47b4b9612c23b359656c15888e3c3597a937f23a
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 20:32:03 -0300

    Filter -> Map -> Reduce

--- collections-pratice/src/Main.java
+++ collections-pratice/src/Main.java
@@ -1,33 +1,37 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Main{
     public static void main(String[] args) {
+        //Stream
+        List<String> funcionarios = List.of("Gabriel","Lucas","Victor","Ana");
 
-        //LIST
-        List<String> funcionarios = new ArrayList<>();
 
-        funcionarios.add("João");
-        funcionarios.add("Maria");
-        funcionarios.add("Pedro");
+        // filter: filtra apenas os nomes que possuem tamanho menor ou igual a 5 caracteres
+        List<String> filtragemNome = funcionarios
+                .stream()
+                .filter(element -> element.length() <= 5).toList();
 
-        System.out.println(funcionarios.get(2));
 
-        //Set
-        Set<String> produtos = new HashSet<>();
+        System.out.println(funcionarios);
+        System.out.println(filtragemNome);
 
-        produtos.add("Agua");
-        produtos.add("H20 limão");
-        produtos.add("Agua");
 
-        System.out.println(produtos );
+        List<Double> valorVendas = List.of(500.0,400.0,200.0);
 
-        //Map
-        Map<Integer,String> cliente = new HashMap<>();
+        // map: transforma cada valor da lista
+        List<Double> comissao = valorVendas
+                .stream()
+                .map(valor -> (valor * 0.05)).toList();
 
-        cliente.put(1,"Gabriel");
-        cliente.put(2,"Lucas");
-        cliente.put(3,"Sérgio");
+        System.out.println(valorVendas);
+        System.out.println(comissao);
 
-        System.out.println(cliente);
+        // reduce: reduz todos os valores da lista para um único valor
+        double totalVendas = valorVendas
+                .stream()
+                .reduce(0.0,Double::sum);
+
+        System.out.println("Total valor das vendas: " + totalVendas);
     }
 }
