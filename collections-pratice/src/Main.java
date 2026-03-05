a2c455c9a12315f3c1740a8e08569bfc48cd2757
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 20:15:29 -0300

    Coleções

--- collections-pratice/src/Main.java
+++ collections-pratice/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 class Main{
     public static void main(String[] args) {
 
+        //LIST
+        List<String> funcionarios = new ArrayList<>();
+
+        funcionarios.add("João");
+        funcionarios.add("Maria");
+        funcionarios.add("Pedro");
+
+        System.out.println(funcionarios.get(2));
+
+        //Set
+        Set<String> produtos = new HashSet<>();
+
+        produtos.add("Agua");
+        produtos.add("H20 limão");
+        produtos.add("Agua");
+
+        System.out.println(produtos );
+
+        //Map
+        Map<Integer,String> cliente = new HashMap<>();
+
+        cliente.put(1,"Gabriel");
+        cliente.put(2,"Lucas");
+        cliente.put(3,"Sérgio");
+
+        System.out.println(cliente);
     }
 }
