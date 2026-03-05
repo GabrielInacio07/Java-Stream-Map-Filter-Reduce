54fe5316f20a333789de2a6e2ec1513943ac4034
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 20:39:37 -0300

    Ex01 - Exibindo elementos de uma lista

--- collections-pratice/src/Main.java
+++ collections-pratice/src/Main.java
@@ -3,35 +3,10 @@ import java.util.List;
 
 class Main{
     public static void main(String[] args) {
-        //Stream
-        List<String> funcionarios = List.of("Gabriel","Lucas","Victor","Ana");
+        System.out.println("Ex01");
 
+        List<String> usuarios = List.of("Admin","Gabriel","João");
 
-        // filter: filtra apenas os nomes que possuem tamanho menor ou igual a 5 caracteres
-        List<String> filtragemNome = funcionarios
-                .stream()
-                .filter(element -> element.length() <= 5).toList();
-
-
-        System.out.println(funcionarios);
-        System.out.println(filtragemNome);
-
-
-        List<Double> valorVendas = List.of(500.0,400.0,200.0);
-
-        // map: transforma cada valor da lista
-        List<Double> comissao = valorVendas
-                .stream()
-                .map(valor -> (valor * 0.05)).toList();
-
-        System.out.println(valorVendas);
-        System.out.println(comissao);
-
-        // reduce: reduz todos os valores da lista para um único valor
-        double totalVendas = valorVendas
-                .stream()
-                .reduce(0.0,Double::sum);
-
-        System.out.println("Total valor das vendas: " + totalVendas);
+        System.out.println(usuarios);
     }
 }
