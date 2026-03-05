cc9e12a78305d58ce1960d385f672c06571f2328
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 20:59:47 -0300

    Ex02 - Collections busca de usuário por posição usando ArrayList

--- collections-pratice/src/Main.java
+++ collections-pratice/src/Main.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Ex01");
+        System.out.println("Ex03");
 
-        List<String> usuarios = List.of("Admin","Gabriel","João");
+        List<String> usuarios = new ArrayList<>(List.of("Admin", "Gabriel", "João"));
 
-        System.out.println(usuarios);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite qual a colocação de usuário que deseja buscar: ");
+        int posicao = Integer.parseInt(input.nextLine()) -1;
+
+        if(posicao >= usuarios.size() || posicao < 0){
+            throw new IllegalArgumentException("[Inválido] posição não coerente número de usuários: " + usuarios.size());
+        }
+
+        String busca = usuarios.get(posicao);
+        System.out.println("Usuário encontrado: " + busca);
+        System.out.println("Total de usuários: " + usuarios.size());
+
+
+        input.close();
     }
 }
