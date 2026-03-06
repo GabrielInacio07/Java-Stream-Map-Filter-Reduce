b6efd46f1763e2af8874c26848880d52bba35f80
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 22:58:36 -0300

    Ex04 - Verificando duplicidade em lista

--- collections-pratice/src/Main.java
+++ collections-pratice/src/Main.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Ex03");
+        System.out.println("Ex04");
 
-        List<String> usuarios = new ArrayList<>(List.of("Admin", "Gabriel", "João"));
+        Set<String> eventos = new HashSet<>();
 
-        Scanner input = new Scanner(System.in);
+        eventos.add("IA Conference Brasil");
+        eventos.add("AI Summit");
+        eventos.add("DevFest");
+        eventos.add("Cloud Expo");
+        eventos.add("IA Conference Brasil");
+        eventos.add("DevFest");
 
-        System.out.print("Digite qual a colocação de usuário que deseja buscar: ");
-        int posicao = Integer.parseInt(input.nextLine()) -1;
-
-        if(posicao >= usuarios.size() || posicao < 0){
-            throw new IllegalArgumentException("[Inválido] posição não coerente número de usuários: " + usuarios.size());
-        }
-
-        String busca = usuarios.get(posicao);
-        System.out.println("Usuário encontrado: " + busca);
-        System.out.println("Total de usuários: " + usuarios.size());
-
-
-        input.close();
+        System.out.println("Lista de Eventos: " + eventos);
     }
 }
